f6a5194a05046c93f123e5d8c5e5422fa9151d8d
ahmed-abdulraziq <ahmed.m.abdulraziq@gmail.com>
2023-03-07 20:35:21 +0200

    up config

--- udagram-api/set_env.sh
+++ udagram-api/set_env.sh
@@ -1,1 +1,12 @@
-eb setenv POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgres POSTGRES_HOST=database-4.cqurqmeyaeal.us-east-1.rds.amazonaws.com POSTGRES_DB=postgres AWS_BUCKET=arn:aws:s3:::front-udagram-453990333417 AWS_REGION=us-east-1 AWS_DEFAULT_REGION=us-east-1 AWS_PROFILE=default JWT_SECRET=mysecretstring URL=http://front-udagram.s3-website-us-east-1.amazonaws.com AWS_ACCESS_KEY_ID=AKIAWTM7OXPUWE2FMRPH AWS_SECRET_ACCESS_KEY=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
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-4.cqurqmeyaeal.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::front-udagram-453990333417
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://front-udagram.s3-website-us-east-1.amazonaws.com
+export AWS_ACCESS_KEY_ID=AKIAWTM7OXPUWE2FMRPH
+export AWS_SECRET_ACCESS_KEY=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
+export AWS_DEFAULT_REGION=us-east-1
